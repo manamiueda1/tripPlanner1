d78db51bb1574f053c67cef41057003e62737a9b
Manami Ueda <manamiueda1@gmail.com>
2019-11-18 12:56:53 -0500

    doing css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const mapboxgl = require("mapbox-gl/dist/mapbox-gl.js");
-const buildMark = require('./marker');
+const buildMark = require("./marker");
 
-mapboxgl.accessToken = "pk.eyJ1IjoibWFuYW1pdWVkYSIsImEiOiJjazE2bGhweXIxNndrM2RvM3ZlcjJ2OWN0In0.BFH1qaAMg_hBCLErNqC3QQ";
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibWFuYW1pdWVkYSIsImEiOiJjazE2bGhweXIxNndrM2RvM3ZlcjJ2OWN0In0.BFH1qaAMg_hBCLErNqC3QQ";
 
 let map = new mapboxgl.Map({
   center: [-74.009, 40.705],
@@ -10,12 +11,14 @@ let map = new mapboxgl.Map({
   style: "mapbox://styles/mapbox/streets-v11"
 });
 
-const fsa = 'Activity';
+map.addControl(new mapboxgl.NavigationControl());
+
+const fsa = "Activity";
 const coord = [-74.009, 40.705];
-const cop = 'Food';
-const coord1 = [-74.0000092,40.7388958];
-const whit = 'Hotel';
-const coord2 = [-73.9757414,40.7627161];
+const cop = "Food";
+const coord1 = [-74.0000092, 40.7388958];
+const whit = "Hotel";
+const coord2 = [-73.9757414, 40.7627161];
 
 const FSA = buildMark(fsa, coord, map);
 const Coppelia = buildMark(cop, coord1, map);
